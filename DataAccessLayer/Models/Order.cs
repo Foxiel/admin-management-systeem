9bf694999e3efb039c68b66b1f97707912cda479
Tristan Liska <Tristanliska0412@gmail.com>
2026-06-07 20:30:53 +0200

    eerste werkende versie
    Sizing is nog niet helemaal goed maar alles werkt

--- DataAccessLayer/Models/Order.cs
+++ DataAccessLayer/Models/Order.cs
@@ -1,6 +1,8 @@
 ﻿//Gemaakt door Tristan
 //Aangepast door Fabian, velden aangepast en vertaald
 
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace DataAccessLayer.Models;
@@ -9,8 +11,15 @@ public class Order
 {
     [Key]
     public int Id { get; set; }
-    public Customer Klant { get; set; }
-    public List<Product> Producten { get; set; }
+
+    public Customer? Klant { get; set; }
+
+    // bewaar voor compatibiliteit (kan leeg zijn als we Bestelregels gebruiken)
+    public List<Product>? Producten { get; set; }
+
+    // bestellijnen met aantallen
+    public List<OrderItem>? Bestelregels { get; set; }
+
     public DateTime BestelDatum { get; set; }
-    public string BestelStatus { get; set; }
+    public string BestelStatus { get; set; } = string.Empty;
 }
---
+++ DataAccessLayer/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        // aantal per regel
+        public int Aantal { get; set; }
+
+        // gekoppeld Product (gevuld door repository)
+        public Product? Product { get; set; }
+    }
+}
